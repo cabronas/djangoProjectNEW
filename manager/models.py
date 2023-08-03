f2ce1111076dae85b0b8071dbb504a75883cf082
user <cabronian123@gmail.com>
2023-08-03 21:38:30 +0300

    Views

--- manager/models.py
+++ manager/models.py
@@ -10,6 +10,7 @@ class Book(models.Model):
     title = models.CharField(max_length=100, verbose_name='Book title')
     text = models.TextField(verbose_name='Book text')
     date = models.DateTimeField(auto_now=True, verbose_name='Addition date')
+    author = models.ManyToManyField(User, related_name="books")
 
     def __str__(self):
         return self.title
@@ -18,8 +19,8 @@ class Book(models.Model):
 class Comment(models.Model):
     text = models.TextField(verbose_name='Comment text')
     date = models.DateTimeField(auto_now=True, verbose_name='Comment date')
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, null=True, blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, null=True, blank=True, related_name="comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
 
     def __str__(self):
         return self.book.title + " comment " + str(self.id)
